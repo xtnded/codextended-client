7c17d866c7951cf71015b664cdad406b4303c6cc
Defected <lil.funk.133@gmail.com>
2021-02-27 13:04:50 +0100

    some Discord "Join" code, cleanup

--- discord.cpp
+++ discord.cpp
@@ -1,34 +1,16 @@
 #include "shared.h"
 #include "client.h"
-// #pragma comment(lib, "dependencies/discord/discord-rpc.lib")
 #include "dependencies/discord/discord_rpc.h"
 #include "dependencies/discord/discord_register.h"
 
-/*
-#include <windows.h>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <functional>
-
-void CL_DiscordInitialize();
-void CL_DiscordFrame();
-void CL_DiscordShutdown();
-*/
+// Reference: https://github.com/callofduty4x/CoD4x_Client_pub
 
 struct discordData_s {
 	unsigned int nextupdatetime;
-	/* char joinSecret[256]; // cod4x
-	char partyId[33];
-	int privclients;
-	int maxclients;Discord_StatusDemo
-	qboolean privaccess;
-	struct pendingJoinRequests_s requests[3];
-	int activerequests;
-	int activeselected; */
+	// char joinSecret[256];
+	// char partyId[33];
+	// int privclients;
+	// int maxclients;
 };
 static struct discordData_s discordData;
 
@@ -75,12 +57,18 @@ void Discord_StatusPlaying() {
 	discordPresence.largeImageKey = mapname;
 	discordPresence.largeImageText = mapname;
 
+	// Discord_SetJoinSecret();
+	// discordPresence.partyId = discordData.partyId;
+	// discordPresence.partySize = 1;
+	// discordPresence.partyMax = 32;
+	// if (discordData.joinSecret[0]) discordPresence.joinSecret = discordData.joinSecret;
+
 	Discord_UpdatePresence(&discordPresence);
 }
 
 void Discord_Update() {
 	if (discordData.nextupdatetime < *cls_realtime) {
-		discordData.nextupdatetime = *cls_realtime + 5 * 1000; // 5 seconds
+		discordData.nextupdatetime = *cls_realtime + 10 * 1000; // 10 seconds
 
 		if (*clc_demoplaying) {
 			Discord_StatusDemo();
@@ -95,6 +83,8 @@ void Discord_Update() {
 void CL_DiscordInitialize() {
 	DiscordEventHandlers handlers;
 	memset(&handlers, 0, sizeof(handlers));
+	// handlers.joinGame = Discord_JoinGame;
+	// handlers.joinRequest = Discord_JoinRequest;
 	Discord_Initialize("804794761696509972", &handlers, 1, NULL);
 
 	Discord_StatusIdle();
@@ -107,3 +97,102 @@ void CL_DiscordFrame() {
 void CL_DiscordShutdown() {
 	Discord_Shutdown();
 }
+
+/* // Code for game invites, "Join", won't bother with this for now.
+void Discord_SetJoinSecret() {
+	int i, y;
+	y = 0;
+	netadr_t serverAddress = *clc_serverAddress;
+	qboolean bad = qfalse;
+	char joinSecret[256];
+
+	strcpy(joinSecret, "DISCORD");
+	y = 7;
+	if (serverAddress.type == NA_IP) {
+		joinSecret[y] = '4';
+		++y;
+		for (i = 0; i < 4; ++i) {
+			sprintf(&joinSecret[y], "%02X", serverAddress.ip[i]);
+			y += 2;
+		}
+	} else {
+		bad = qtrue;
+	}
+
+	for (i = 0; i < 2; ++i) {
+		sprintf(&joinSecret[y], "%02X", ((byte*)(&serverAddress.port))[i]);
+		y += 2;
+	}
+
+	joinSecret[y] = 0;
+	if (bad) {
+		discordData.joinSecret[0] = 0;
+	} else {
+		Q_strncpyz(discordData.joinSecret, joinSecret, sizeof(discordData.joinSecret));
+	}
+
+	const char* info = clc_stringData + clc_stringOffsets[0];
+	const char* nummaxcl = Info_ValueForKey(info, "sv_maxclients");
+	const char* numprivcl = Info_ValueForKey(info, "sv_privateclients");
+	discordData.maxclients = atoi(nummaxcl);
+	discordData.privclients = atoi(numprivcl);
+	discordData.nextupdatetime = *cls_realtime + 100;
+}
+
+void Discord_JoinGame(const char* joinSecret) {
+	printf("Discord: join (%s)\n", joinSecret);
+
+	int k;
+	int i = 0;
+	netadr_t adr;
+	char password[128];
+	char cmdline[1024];
+	char hexbyte[3];
+	unsigned int scanbyte;
+
+	memset(&adr, 0, sizeof(adr));
+
+	if (strncmp(joinSecret, "DISCORD", 7) != 0) {
+		Com_Printf("Bad joinSecret %d\n", i);
+		return;
+	}
+	i += 7;
+	int type = joinSecret[i];
+	++i;
+	hexbyte[2] = 0;
+	if (type == '4') {
+		if (strlen(joinSecret + i) < 10) {
+			Com_Printf("Bad joinSecret %d\n", i);
+			return;
+		}
+		for (k = 0; k < 4; ++k) {
+			hexbyte[0] = joinSecret[i];
+			hexbyte[1] = joinSecret[i + 1];
+
+			sscanf(hexbyte, "%02X", &scanbyte);
+			adr.ip[k] = scanbyte;
+			i += 2;
+		}
+		adr.type = NA_IP;
+	} else {
+		Com_Error(ERR_DROP, "Sorry, but this discord join method is not implemented yet.");
+		return;
+	}
+	for (k = 0; k < 2; ++k) {
+		hexbyte[0] = joinSecret[i];
+		hexbyte[1] = joinSecret[i + 1];
+		sscanf(hexbyte, "%02X", &scanbyte);
+		((byte*)(&adr.port))[k] = scanbyte;
+		i += 2;
+	}
+
+	Com_sprintf(cmdline, sizeof(cmdline), "connect %s\n", NET_AdrToString(adr));
+	CL_AddReliableCommand(cmdline);
+	Cvar_Set("testingggg", cmdline);
+}
+
+void Discord_JoinRequest(const DiscordUser* request) {
+	Com_Printf("Discord: join request from %s#%s - %s\n", request->username, request->discriminator, request->userId);
+	Discord_Respond(request->userId, DISCORD_REPLY_YES);
+}
+*/
--- dllmain.cpp
+++ dllmain.cpp
@@ -4,19 +4,12 @@ void codextended();
 void codextended_unload();
 
 HMODULE hModule;
-#if 1
 HANDLE hLogFile = INVALID_HANDLE_VALUE;
-#endif
 
-/* PROCESS_DETACH is not called so don't make global declarations which have deconstructors which have to be called.
-*/
+// PROCESS_DETACH is not called so don't make global declarations which have deconstructors which have to be called.
 
 #ifndef PROJECT_EXE
-
-BOOL APIENTRY DllMain( HMODULE hMod,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hMod, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	char szModuleName[MAX_PATH + 1];
 
@@ -26,31 +19,17 @@ BOOL APIENTRY DllMain( HMODULE hMod,
 		return TRUE;
 	}
 
-	switch (ul_reason_for_call)
-	{
-		case DLL_PROCESS_ATTACH: {
+	switch (ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+			// _chdir("C:\\Games\\Call of Duty"); // Fix for Discord RPC "Join", it shouldn't be hardcoded though.
 			DisableThreadLibraryCalls(hMod);
 			hModule = hMod;
-	#if 0
-	#ifdef DEBUG
-			if (hLogFile == INVALID_HANDLE_VALUE) {
-				hLogFile = CreateFile("./cod_mem_log.txt", GENERIC_WRITE,
-					FILE_SHARE_WRITE, NULL, CREATE_ALWAYS,
-					FILE_ATTRIBUTE_NORMAL, NULL);
-				_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-				_CrtSetReportFile(_CRT_WARN, hLogFile);
-			}
-	#endif
-	#endif
-			
 			codextended();
-		}
-								 break;
-		case DLL_PROCESS_DETACH: {
-			//codextended_unload();
-		} break;
+		break;
+		case DLL_PROCESS_DETACH:
+			// codextended_unload();
+		break;
 	}
 	return TRUE;
 }
-
 #endif
